9c3802d50d80e67cb58c2e39a947cbe2599cb0e4
preludeinc <j.guenter@protonmail.com>
2025-02-26 18:56:33 -0800

    Updated tests

--- blog/playwright/tests/bloc_app.spec.js
+++ blog/playwright/tests/bloc_app.spec.js
@@ -1,3 +1,4 @@
+import { loginWith, createBlog } from './helper'
 const { test, expect, beforeEach, describe } = require('@playwright/test')
 
 describe('Blog app', () => {
@@ -8,31 +9,56 @@ describe('Blog app', () => {
   test('Login form is shown', async ({ page }) => {
     await expect(page).toHaveTitle(/Blog App/)
 
-    const usernameLocator = await page.getByText('username')
-    await expect(usernameLocator).toBeVisible()
-
-    const passLocator = await page.getByText('password')
-    await expect(passLocator).toBeVisible()
+    await expect(page.getByText('username')).toBeVisible()
+    await expect(page.getByText('password')).toBeVisible()
   })
 
   describe('Login', () => {
     test('succeeds with correct credentials', async ({ page }) => {
-      const textboxes = await page.getByRole('textbox').all()
-      await textboxes[0].fill('preludeinc')
-      await textboxes[1].fill('babbage')
+      await loginWith(page, 'preludeinc', 'babbage')
 
-      await page.getByRole('button', { name: 'login' }).click()
-
-      await expect(page.getByText('Ada Lovelace logged in')).toBeVisible();
+      await expect(page.getByText('Ada Lovelace logged in')).toBeVisible()
     })
 
     test('fails with wrong credentials', async ({ page }) => {
-      const textboxes = await page.getByRole('textbox').all()
-      await textboxes[0].fill('prelude')
-      await textboxes[1].fill('babbage')
+      await loginWith(page, 'prelude', 'babbage')
 
       await page.getByRole('button', { name: 'login' }).click()
-      await expect(page.getByText('Wrong username or password')).toBeVisible();
+      await expect(page.getByText('Wrong username or password')).toBeVisible()
+    })
+  })
+
+  describe('When logged in', () => {
+    beforeEach(async ({ page }) => {
+      await loginWith(page, 'preludeinc', 'babbage')
+      await page.getByRole('button', { name: 'new blog' }).click()
+    })
+
+    test('a new blog can be created', async ({ page }) => {
+      await createBlog(page, 'SCP Foundation', 'Placeholder', 'https://scp-wiki.wikidot.com/scp-ex', '13')
+      await expect(page.getByText('SCP Foundation').first()).toBeVisible()
+    })
+  })
+
+  describe('A blog can be liked', () => {
+    beforeEach(async ({ page }) => {
+      await loginWith(page, 'preludeinc', 'babbage')
+    })
+
+    test('once the like button is clicked', async ({ page }) => {
+      await page.getByRole('button', { name: 'view details' }).first().click()
+      await page.getByRole('button', { name: 'likes' }).click()
+    })
+  })
+
+  describe('A blog can be removed', () => {
+    beforeEach(async ({ page }) => {
+      await loginWith(page, 'preludeinc', 'babbage')
+    })
+
+    test('once the remove button is clicked', async ({ page }) => {
+      await page.getByRole('button', { name: 'view details' }).first().click()
+      await page.getByRole('button', { name: 'remove' }).click()
     })
   })
 })
